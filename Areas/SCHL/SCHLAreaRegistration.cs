4b714c6cee11563c4e5b3c56fd9133c0f446fa9c
MaMo1993 <60958581+MaMo1993@users.noreply.github.com>
2020-06-06 17:03:16 +0200

    Add files via upload

---
+++ Areas/SCHL/SCHLAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace PCMS_Application.Areas.SCHL
+{
+    public class SCHLAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "SCHL";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "SCHL_default",
+                "SCHL/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
