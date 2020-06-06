5d56940adcfb6d0c491b7b6ba646277302ce5586
MaMo1993 <60958581+MaMo1993@users.noreply.github.com>
2020-06-06 17:54:19 +0200

    Add files via upload

---
+++ Areas/GURDN/GURDNAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace PCMS_Application.Areas.GURDN
+{
+    public class GURDNAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "GURDN";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "GURDN_default",
+                "GURDN/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
